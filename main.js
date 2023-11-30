2ba47e65b7722ef71438df29e0b7676b3088a1c9
kuro299 <149150290+kuro299@users.noreply.github.com>
2023-11-30 14:31:21 +0100

    sign up done

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
-function makeClientOrDoctor(email,password,selected){
+function generateID() {
+    var count = 0;
+    return function () {
+      return count++;
+    };
+  }
+  var id = generateID();
+
+function makePatient(name,lastname,email,password){
  var obj={}
+ obj.id=id()
+ obj.name=name
+ obj.lastname=lastname
  obj.email=email
  obj.password=password
- obj.selected=selected
  return obj
 }
-function MakeClientOrDoctor(){
+function Makedoctor(email,pass){
     var obj ={}
+    obj.email=email
+    obj.pass=pass
     obj.add=add
-    obj.listdoctors=[]
     obj.listpatients=[]
     return obj
 }
-var add=function(email,password,selected){
-    var result=makeClientOrDoctor(email,password,selected)
-    if(selected==="doctor"){
-    this.listdoctors.push(result)}
+var add=function(name,lastname,email,password){
+var result=makePatient(name,lastname,email,password)
+this.listpatients.push(result)
 
-    if(selected==="patient"){
-    this.listpatients.push(result)}
     }
 
-var p=MakeClientOrDoctor()
+var p=Makedoctor()
 
 $("#create-account").on("click", function() {
+const name=$("#name").val()
+const lastname=$("#lastname").val()
 const email=$("#email").val()
 const password=$("#password").val()
-var selected = $('input[name="one"]:checked').val();
-p.add(email,password,selected)
-window.location.href="login.html"
+p.add(name,lastname,email,password)
+console.log(p)
 });
-
+$("#create-account").on("click", function() {
+var DoctorJason = JSON.stringify(p);
+localStorage.setItem('doctor', DoctorJason);
+console.log(DoctorJason)
+});
